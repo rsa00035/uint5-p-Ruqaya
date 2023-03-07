736bb617b1eef7981874dc7e158b87c0cd725590
rsa00035 <98711191+rsa00035@users.noreply.github.com>
2023-03-07 03:08:35 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package mvcexample;
+
+/**
+ *
+ * @author savi
+ */
+// Launcher for application
+
+public class Main {
+   
+    public static void main(String args[]) {
+     
+        Model model = new Model();               // Create shared database
+        Controller1 c1 = new Controller1(model); // Create first controller
+        Controller2 c2 = new Controller2(model); // Create second controller
+                 
+        Controller2 c3 = new Controller2(model); 
+        Controller2 c4 = new Controller2(model);
+    } // main
+  
+} // Main
